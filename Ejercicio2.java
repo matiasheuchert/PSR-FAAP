2af589e93763fbdd61e067136d3663623d738403
Matias Heuchert <73125441+matiasheuchert@users.noreply.github.com>
2024-05-08 12:08:29 -0300

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Número  Cuadrado  Cubo");
+		int i;
+		
+		for(i=0; i<11; i++) {
+			
+		System.out.println(i + "       " + i*i + "         " + i*i*i);	
+		
+		}
+		
+
+	}
+
+}

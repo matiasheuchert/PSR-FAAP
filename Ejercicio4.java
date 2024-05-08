f32c5f1ebb0a4b4ed8698449b737327bf4b7cda5
Matias Heuchert <73125441+matiasheuchert@users.noreply.github.com>
2024-05-08 15:13:19 -0300

    Create Ejercicio4.java

---
+++ Ejercicio4.java
@@ -0,0 +1,56 @@
+package ejercicio1;
+
+public class Ejercicio4 {
+	
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=30; i<46; i++) {
+				
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=46; i<61; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=61; i<76; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=76; i<91; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=91; i<106; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=106; i<121; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		System.out.println();
+		for(i=121; i<127; i++) {
+			
+			System.out.print((char) i + " ");
+		}
+		
+		
+		
+		
+		
+	}
+
+}
